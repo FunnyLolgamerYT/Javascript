86f26aeb3822de2ed962bb60b7611bac091d9582
Yarno de Jong <yarnodjong@outlook.com>
2023-01-19 10:40:32 +0100

    continuing

---
+++ Lectures/starter/script.js
@@ -0,0 +1,34 @@
+
+//Lecture Values and Variables
+const country = "The Netherlands"; 
+const continent = "Europe";
+let population
+population = 17500000
+/*
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
+
+//Lecture Data Types
+
+const isIsland = false;
+let language;
+console.log(isIsland)
+console.log(population);
+console.log(country);
+console.log(language);
+
+//Lecture let, const and var
+
+language = "Dutch";
+//continent = "The amerikas"
+
+
+//Lecture Basic Operators\
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population > 6000000);
+console.log(population > 33000000);
+const description = "Portugal is in Europe, and its 11 million people speak portugese";
--- starter/script.js
+++ starter/script.js
@@ -34,9 +34,55 @@ year = 2023;
 console.log(typeof year);
 
 console.log(typeof null);
-*/
+
 
 let age = 30;
 age = 31;
 const birthYear = 1991;
-const job = "hehehehaw";
+//const job = "hehehehaw";
+const yearNow = 2023;
+const ageYarno = yearNow - 2006;
+const ageFaith = yearNow - 2005;
+var job = 'programmer';
+console.log(ageYarno, ageFaith);
+
+const firstName = 'yarno';
+const lastName = 'de Jong';
+
+console.log(firstName +  ' ' +  lastName);
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x ++;
+x--;
+x--;
+console.log(x);
+
+let aman = 'cringe';
+
+console.log(aman);
+
+aman = 16;
+console.log(aman);
+
+console.log(ageYarno >= 18);
+
+const isFullAge = ageYarno >= 18;
+
+console.log(now - yearNow > now - 2006)
+*/
+
+const now = 2023;
+const ageYarno = now - 2006;
+const ageFaith = now - 2005;
+
+console.log( ageYarno > ageFaith)
+console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25-10-5;
+console.log(x,y);
+
+
+const averageAge = (ageYarno + ageFaith) / 2;
+console.log(ageFaith, ageYarno, averageAge)
