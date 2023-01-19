7628662bb547889dfe02ffe1e860cac4f387f01a
Yarno de Jong <yarnodjong@outlook.com>
2023-01-19 09:21:40 +0100

    update javascript

---
+++ starter/script.js
@@ -0,0 +1,42 @@
+let js = "amazing";
+//console.log(40 + 8 + 23 - 10);
+
+/*
+console.log('Yarno');
+console.log(16);
+
+let firstName = "Yarno";
+console.log(firstName);
+
+// Different variables
+let aNumber = 4;
+let aSring = 'words'
+let aBoolean = true;
+let anUndefined;
+
+true;
+console.log(true);
+let javaScriptIsFun = true;
+
+//console.log(typeof javaScriptIsFun)
+console.log(typeof true);
+//console.log(typeof 23);
+//console.log(typeof 'Yarno');
+
+javaScriptIsFun = "yes";
+console.log(typeof javaScriptIsFun)
+
+let year;
+console.log(year)
+console.log(typeof year)
+
+year = 2023;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+let age = 30;
+age = 31;
+const birthYear = 1991;
+const job = "hehehehaw";
