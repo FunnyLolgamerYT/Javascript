4a6918d93e3e898ba98548787e57d115ef3f5ab9
EssentialBee <yarnodjong@outlook.com>
2023-03-06 10:18:40 +0100

    Hon hon

---
+++ Week 1 sectie 2/Document 1/script.js
@@ -0,0 +1,11 @@
+const rich = false;
+const happy = true;
+const smart = true;
+
+
+if (rich) {
+    console.log("you are not a teacher!");
+
+} else if(happy && smart) {
+    console.log("You do something you like, probably in the public sector")
+}
